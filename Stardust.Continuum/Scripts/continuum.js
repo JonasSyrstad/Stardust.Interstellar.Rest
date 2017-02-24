35651563f7850857c2d3b1a0086a5ed7db77948e
Jonas Syrstad <jonassyrstad@outlook.com>
2017-02-24 15:35:40 +0100

    added keyboard shortcuts for full screen, clear stream and pause/resume

--- Stardust.Continuum/Scripts/continuum.js
+++ Stardust.Continuum/Scripts/continuum.js
@@ -1,23 +1,25 @@
 ﻿$.UseContinuum = function (rootUrl, project, environment, serviceName, apiKey) {
+    if (!$.correlationToken) $.correlationToken = "";
+    if (!$.continuumUsernam) $.continuumUsernam = "";
+    var apiUrl = rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey;
     window.console = (function (rootUrl, project, environment, serviceName, apiKey) {
         var origConsole = window.console;
-        var put =
-            jQuery[method] = function (url, data, callback, type) {
-                if (jQuery.isFunction(data)) {
-                    type = "PUT",
-                        callback = data;
-                    data = undefined;
-                }
-                if (callback === null) callback = function () { };
-                return jQuery.ajax({
-                    url: url,
-                    type: "PUT",
-                    method: "PUT",
-                    dataType: 'json',
-                    contentType: 'application/json; charset=utf-8',
-                    data: JSON.stringify(data)
-                });
-            };
+        var put = function (url, data, callback, type) {
+            if (jQuery.isFunction(data)) {
+                type = "PUT",
+                    callback = data;
+                data = undefined;
+            }
+            if (callback === null) callback = function () { };
+            return jQuery.ajax({
+                url: url,
+                type: "PUT",
+                method: "PUT",
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            });
+        };
         if (!window.console || !origConsole)
             origConsole = {};
         var isDebug = false,
@@ -31,46 +33,47 @@
             log: function () {
                 origConsole.log && origConsole.log.apply(origConsole, arguments);
                 //debugger; 
-                put(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey,
-                    {
-                        UserName: $.continuumUsernam,
-                        CorrelationToken: correlationToken,
-                        Message: arguments[0],
-                        ServiceName: serviceName,
-                        Environment: environment,
-                        Properties: {}
-                    }).then(function (result) {
+                put(apiUrl,
+                {
+                    UserName: $.continuumUsernam,
+                    CorrelationToken: $.correlationToken,
+                    Message: arguments[0],
+                    ServiceName: serviceName,
+                    Environment: environment,
+                    Properties: {}
+                }).then(function (result) {
 
-                    },
-                   function (error) {
-                       origConsole.error(error);
-                       origConsole.log(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey);
-                   });;
+                },
+                    function (error) {
+                        origConsole.error(error);
+                        origConsole.log(apiUrl);
+                    });;
             },
             warn: function () {
                 origConsole.warn && origConsole.warn.apply(origConsole, arguments);
-                put(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey,
-               {
-                   UserName: $.continuumUsernam,
-                   CorrelationToken: correlationToken,
-                   Message: arguments[0],
-                   ServiceName: serviceName,
-                   Environment: environment,
-                   Properties: {}
-               }).then(function (result) {
+                put(apiUrl,
+                {
+                    UserName: $.continuumUsernam,
+                    CorrelationToken: $.correlationToken,
+                    Message: arguments[0],
+                    ServiceName: serviceName,
+                    Environment: environment,
+                    Properties: {}
+                }).then(function (result) {
 
-               },
-                   function (error) {
-                       origConsole.error(error);
-                       origConsole.log(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey);
-                   });;
+                },
+                    function (error) {
+                        origConsole.error(error);
+                        origConsole
+                            .log(apiUrl);
+                    });;
             },
             error: function () {
                 origConsole.error && origConsole.error.apply(origConsole, arguments);
-                put(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey,
+                put(apiUrl,
                 {
                     UserName: $.continuumUsernam,
-                    CorrelationToken: correlationToken,
+                    CorrelationToken: $.correlationToken,
                     Message: arguments[0].split('\n')[0],
                     StackTrace: arguments[0],
                     ServiceName: serviceName,
@@ -81,16 +84,17 @@
                 },
                     function (error) {
                         origConsole.error(error);
-                        origConsole.log(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey);
+                        origConsole
+                            .log(apiUrl);
                     });;
             },
             info: function () {
 
                 origConsole.info && origConsole.info.apply(origConsole, arguments);
-                put(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey,
+                put(apiUrl,
                 {
                     UserName: $.continuumUsernam,
-                    CorrelationToken: correlationToken,
+                    CorrelationToken: $.correlationToken,
                     Message: arguments[0],
                     ServiceName: serviceName,
                     Environment: environment,
@@ -98,14 +102,15 @@
                 }).then(function (result) {
 
                 },
-                  function (error) {
-                      origConsole.error(error);
-                      origConsole.log(rootUrl + "/api/v1/single/" + project + "/" + environment + "?api_key=" + apiKey);
-                  });;
+                    function (error) {
+                        origConsole.error(error);
+                        origConsole
+                            .log(apiUrl);
+                    });;
             },
             debug: function (bool) {
                 isDebug = bool;
-            },
+            }
 
         };
 
